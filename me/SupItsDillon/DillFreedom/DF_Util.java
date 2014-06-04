7683ae5f200b07b2163a398bfc27a8e8e8624c45
Robolord <overhypedlord@gmail.com>
2014-06-04 17:30:18 +0100

    Util ;3
    Made a basic Util

---
+++ me/SupItsDillon/DillFreedom/DF_Util.java
@@ -0,0 +1,53 @@
+package me.SupItsDillon.DillFreedom;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import me.StevenLawson.TotalFreedomMod.TFM_SuperadminList;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+
+public class DF_Util
+{
+  private static final Logger logger = Logger.getLogger("Minecraft");
+
+  private DF_Util()
+  {
+    throw new AssertionError();
+  }
+
+  public static void bcastMsg(String message)
+  {
+    logger.info(message);
+
+    for (Player p : Bukkit.getOnlinePlayers())
+    {
+      p.sendMessage(message);
+    }
+  }
+
+    catch (Exception ex)
+    {
+    }
+
+    return false;
+  }
+
+  public static void log(Level level, String message)
+  {
+    logger.log(level, message);
+  }
+
+  public static String maskIpAddress(String address, CommandSender sender)
+  {
+    if (isUserSuperadmin(sender))
+    {
+      return address;
+    }
+
+    String[] address_parts = address.split("\\.");
+    return address_parts[0] + "." + address_parts[1] + ".*.*";
+  }
+}
